b876324ecf0ae82d67c44bf42208c50e50d31c6f
Lucca <luccajbecker@gmail.com>
2024-04-26 23:24:17 -0300

    fix

--- routes/teamSearch.py
+++ routes/teamSearch.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 import requests
+import statbotics
 
 teamSearch = Blueprint('teamSearch', __name__)
 
@@ -12,26 +13,28 @@ def index():
     try:
         team = request.args.get("team")
 
+        sb = statbotics.Statbotics()
+
+        sbResponseGetTeam = sb.get_team(int(team))
         responseTeamInfos = requests.get(TbaApiEndpoint + f"/team/frc{team}", headers=TbaHeaders).json()
         responseTeamEvents = requests.get(TbaApiEndpoint + f"/team/frc{team}/events/2024", headers=TbaHeaders).json()
 
         teamName = responseTeamInfos["nickname"]
         teamCountry = responseTeamInfos["country"]
         teamRookieYear = responseTeamInfos["rookie_year"]
-        teamAttendedEvents = ""
-        
+        teamAttendedEvents = []
+        teamWinrate = sbResponseGetTeam["winrate"]
 
         for event in responseTeamEvents:
-            teamAttendedEvents += event["name"] + "; "
-
-        
+            teamAttendedEvents.append(event["name"])
 
         return render_template("teamSearch.html",
                                teamName=teamName,
                                team=team,
                                teamCountry=teamCountry,
                                rookieYear=teamRookieYear,
-                               teamAttendedEvents=teamAttendedEvents[:-2])
+                               teamAttendedEvents=teamAttendedEvents,
+                               teamWinrate=(str(teamWinrate * 100))[:5] + "%")
     except:
         print("Invalid team")
         return redirect(url_for('errorPage.index', team=team))
--- test.py
+++ test.py
@@ -4,8 +4,17 @@ TbaApiKey = "dPeEI571e5LotL4zsavOhgcehtzq0NP7VJaSDOo3gWCMpL1R4riSYvddhBpZZ4Sw"
 TbaApiEndpoint = "https://www.thebluealliance.com/api/v3"
 TbaHeaders = {"X-TBA-Auth-Key": TbaApiKey}
 
-response = requests.get(TbaApiEndpoint + f"/team/frc1156/events/2024", headers=TbaHeaders).json()
+response = requests.get(TbaApiEndpoint + f"/team/frc1156/matches/2024", headers=TbaHeaders).json()
 #print(response[0])
 
-for i in response:
-    print(i["name"])
+coreData = []
+
+for match in response:
+    coreData.append({
+        "matchType": match["comp_level"],
+        "red": match["alliances"]["red"]["team_keys"],
+        "blue": match["alliances"]["blue"]["team_keys"],
+        "winningAlliance": match["winning_alliance"],
+        })
+    
+print(coreData[8])
