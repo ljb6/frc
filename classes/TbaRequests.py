0bbac27c82b88b6efe5b4baa3e62119e78dd7c34
Lucca <luccajbecker@gmail.com>
2024-04-28 20:47:42 -0300

    Update TbaRequests.py

--- classes/TbaRequests.py
+++ classes/TbaRequests.py
@@ -5,10 +5,14 @@ class TbaRequests():
         self.tbaEndpoint = "https://www.thebluealliance.com/api/v3"
         self.tbaHeaders = {"X-TBA-Auth-Key": apiKey}
 
-    def getTeamEvents(self, team):
+    def getTeamBasicInfo(self, team):
         response = requests.get(self.tbaEndpoint + f"/team/frc{team}", headers=self.tbaHeaders).json()
-        print(response)
+        return {
+            "nickname": response["nickname"],
+            "teamNumber": response["team_number"],
+            "rookieYear": response["rookie_year"],
+            "country": response["country"],
+            "key": response["key"]
+            }
 
 teste = TbaRequests("dPeEI571e5LotL4zsavOhgcehtzq0NP7VJaSDOo3gWCMpL1R4riSYvddhBpZZ4Sw")
-
-teste.getTeamEvents("1156")
