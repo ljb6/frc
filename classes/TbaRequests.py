470fac51b7d8b2f4e9d8b2a3a5ded237d7a2cec7
Lucca <luccajbecker@gmail.com>
2024-04-29 20:00:38 -0300

    class tba

--- classes/TbaRequests.py
+++ classes/TbaRequests.py
@@ -14,5 +14,9 @@ class TbaRequests():
             "country": response["country"],
             "key": response["key"]
             }
+    
+    def getAmpLeverageForEvents(self, events):
+        response = requests.get(self.tbaEndpoint + f"/events/event??", headers=self.tbaHeaders).json()
+
 
 teste = TbaRequests("dPeEI571e5LotL4zsavOhgcehtzq0NP7VJaSDOo3gWCMpL1R4riSYvddhBpZZ4Sw")
