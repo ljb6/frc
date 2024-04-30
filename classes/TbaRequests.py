1ee2868a19e68d5b503c9b56a8d0cea8f20a17b6
Lucca <luccajbecker@gmail.com>
2024-04-29 22:24:47 -0300

    ampLeverage added

--- classes/TbaRequests.py
+++ classes/TbaRequests.py
@@ -15,8 +15,31 @@ class TbaRequests():
             "key": response["key"]
             }
     
-    def getAmpLeverageForEvents(self, events):
-        response = requests.get(self.tbaEndpoint + f"/events/event??", headers=self.tbaHeaders).json()
+    def getEventsAmpLeverage(self, events):
+        counter = 0
+        for event in events:
+            response = requests.get(self.tbaEndpoint + f"/event/{event}/matches??", headers=self.tbaHeaders).json()
+            ampLeverage = []
+            matchCounter = 1
 
+            for match in response:
+                if match["score_breakdown"] is not None:
+                    speakerAmplifiedNotesBlue = match["score_breakdown"]["blue"]["teleopSpeakerNoteAmplifiedCount"]
+                    ampNotesBlue = match["score_breakdown"]["blue"]["teleopAmpNoteCount"]
+                    if ampNotesBlue > 0:
+                        ampLeverage.append({"match": matchCounter, "porcentage": speakerAmplifiedNotesBlue / ((ampNotesBlue / 2) * 4)})
+                if match["score_breakdown"] is not None:
+                    speakerAmplifiedNotesRed = match["score_breakdown"]["red"]["teleopSpeakerNoteAmplifiedCount"]
+                    ampNotesRed = match["score_breakdown"]["red"]["teleopAmpNoteCount"]
+                    if ampNotesRed > 0:
+                        ampLeverage.append({"match": matchCounter, "porcentage": speakerAmplifiedNotesRed / ((ampNotesRed / 2) * 4)})
+                matchCounter += 1
 
-teste = TbaRequests("dPeEI571e5LotL4zsavOhgcehtzq0NP7VJaSDOo3gWCMpL1R4riSYvddhBpZZ4Sw")
+            counter += 1
+            
+        total = 0
+        for metric in ampLeverage:
+            total += metric["porcentage"]
+        
+        leverage = str(total / len(ampLeverage) * 100)
+        return leverage[:5] + "%"
